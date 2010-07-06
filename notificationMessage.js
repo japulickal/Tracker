5c07ae00e8059c7f0a524fa520abbdb692f4da31
Jose Antony <joseantony007@gmail.com>
2010-07-06 22:43:25 +0530

    Moved the trac based logic to seperate file

---
+++ notificationMessage.js
@@ -0,0 +1,5 @@
+var notificationMessage = {
+	title: '',
+	text: '',
+	icon: ''
+};
---
+++ plugins/trac.js
@@ -0,0 +1,49 @@
+/**
+ * variable for holding the trac Plugin
+ * 
+ * Plugin mainly implements the getCount and getNotificationMessages needed
+ * By the main program
+ */
+var tracPlugin = {
+	/**
+	 * This function will return the number of active tickets in the 
+	 * ticket report
+	 * 
+	 * @param xmlDoc xml Document recived from the server
+	 * 
+	 * @returns integer
+	 */
+	getCount: function (xmlDoc) {
+		return xmlDoc.getElementsByTagName('item').length;
+	},
+	/**
+	 * Returns and Message object with details to be displayed if an new ticket
+	 * or comment is added  to the tickets in the given report
+	 * 
+	 * @param xmlDoc xml Document recived from the server
+	 * @param lastRunTime last run time of the script
+	 * 
+	 * @returns notificationMessage
+	 */
+	getNotificationMessages: function (xmlDoc, lastRunTime) {
+		itemCount = xmlDoc.getElementsByTagName('item').length;
+		currentMessageCount = 0;
+		message = new Array();
+		for (count = 0 ; count < itemCount ; count++) {
+			if (undefined != xmlDoc.getElementsByTagName('item')[count].getElementsByTagName('pubDate')[1]) {
+				pubTime = new Date(xmlDoc.getElementsByTagName('item')[count].getElementsByTagName('pubDate')[1].childNodes[0].nodeValue);
+			} else {
+				pubTime = new Date(xmlDoc.getElementsByTagName('item')[count].getElementsByTagName('pubDate')[0].childNodes[0].nodeValue);
+			}
+			if (pubTime >= lastRunTime) {
+				notificationMessage.text = xmlDoc.getElementsByTagName('item')[count].getElementsByTagName('title')[0].childNodes[0].nodeValue;
+				notificationMessage.icon = '/images/Trac_Logo_16.png';
+				notificationMessage.title = 'Ticket / Ticket Comment Added';
+				message[currentMessageCount] = notificationMessage;//xmlDoc.getElementsByTagName('item')[count].getElementsByTagName('title')[0].childNodes[0].nodeValue;
+				currentMessageCount++;
+			}
+		}
+		
+		return message;
+	}
+};
